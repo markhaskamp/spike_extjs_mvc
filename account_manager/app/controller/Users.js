1ea6bf3d9b58bd26a068a5f9b59d32133c80edc7
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-23 14:01:59 -0400

    updated controller init function, added controller onPanelRendered function.

--- account_manager/app/controller/Users.js
+++ account_manager/app/controller/Users.js
@@ -2,7 +2,15 @@ Ext.define('AM.controller.Users', {
     extend: 'Ext.app.Controller',
 
     init: function() {
-        console.log('Initialized Users! This happens before the Application launch function is called');
+        this.control({
+            'viewport > panel': {
+                render: this.onPanelRendered
+            }
+        });
+    },
+
+    onPanelRendered: function() {
+        console.log('The panel was rendered');
     }
 });
 
