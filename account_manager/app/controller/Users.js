74ac801fa4904fea61f63fb10b32f3f859f93d3c
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-23 16:47:02 -0400

    added user/Edit.js view

--- account_manager/app/controller/Users.js
+++ account_manager/app/controller/Users.js
@@ -1,18 +1,21 @@
 Ext.define('AM.controller.Users', {
     extend: 'Ext.app.Controller',
 
-    views: [ 'user.List' ],
+    views: [ 'user.List',
+             'user.Edit'
+    ],
     init: function() {
             
         this.control({
-                'userlist': {
-                        itemdblclick: this.editUser
-                }
+          'userlist': {
+            itemdblclick: this.editUser
+          }
         });
     },
 
     editUser: function(grid, record) {
-                      console.log('Double clicked on ' + record.get('name'));
-              }
+      var view = Ext.widget('useredit');
+      view.down('form').loadRecord(record);
+    }
 });
 
