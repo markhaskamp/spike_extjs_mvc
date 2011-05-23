470b871b27737a5a938e7a84db55d080c731687a
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-23 14:17:07 -0400

    mrh

--- account_manager/app/controller/Users.js
+++ account_manager/app/controller/Users.js
@@ -5,14 +5,14 @@ Ext.define('AM.controller.Users', {
     init: function() {
             
         this.control({
-            'viewport > panel': {
-                render: this.onPanelRendered
-            }
+                'userlist': {
+                        itemdblclick: this.editUser
+                }
         });
     },
 
-    onPanelRendered: function() {
-        console.log('The panel was rendered');
-    }
+    editUser: function(grid, record) {
+                      console.log('Double clicked on ' + record.get('name'));
+              }
 });
 
